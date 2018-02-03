0e4b62b5c5d7ba3b64efb46b6c7cef942d386b6d
Ruslan Pantaev <rpantaev@gmail.com>
2018-02-03 11:41:33 -0500

    Update midi_scraper.py

--- midi_scraper.py
+++ midi_scraper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Author:		Ruslan Pantaev
-# Date:			2017-12-28
+# Author:	Ruslan Pantaev
+# Date:		2017-12-28
 # reference: 	https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 # example site: http://www.midiworld.com/bach.htm
 
